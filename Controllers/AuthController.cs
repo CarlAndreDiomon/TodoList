42b419efa9f4e34344801061b2a3b64d7a2b1aaa
CarlAndreDiomon <dcarlandrerafols@gmail.com>
2025-05-11 17:03:04 +0800

    Initial Commit

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Todo_List_App.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
