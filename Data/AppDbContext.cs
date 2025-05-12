7a99e88cb3f192eb150243b9d6590463040a7d31
CarlAndreDiomon <dcarlandrerafols@gmail.com>
2025-05-12 19:43:08 +0800

    Add database connection string and migration

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Todo_List_App.Data
+{
+    public class AppDbContext : IdentityDbContext<IdentityUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        // Add DbSets for your custom models here
+        // public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
---
+++ Models/Users.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Todo_List_App.Models
+{
+    public class Users : IdentityUser
+    {
+        public string FullName { get; set; }
+    }
+}
